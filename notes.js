fc12c40545eec65e24827f0572eb150bf1d19515
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-10-09 16:27:16 +0500

    created Notes of PIAIC

---
+++ notes.js
@@ -0,0 +1,29 @@
+// https://www.twitter.com/shehza_d_
+
+06-oct-2022
+// https://www.youtube.com/watch?v=qVZPJURw50k 
+// humne class se aagye chalna hy nhiu tw ap khud se na nikal jaye
+// sir zeeshan worlds best developer in blockchain 
+
+08-oct-2022
+// Sat miss hira class
+// youtube developer (isse logic build nhi hota)
+// null has a very specific importance in OOPS based languages where memory pointers are of extremely significant value. Null Checks ensure the program never points to a certain memory space which is simply non-existed 
+// sir please final may safe exam browsewer Ubuntu p kasy install hoga ye batana plz 
+// can some one explain the difference between using + and , in console.log. They give the same output 
+// well done sir imran is great spokesperson of the students well done sir keep it up 
+// Jawwad 03037309909 
+// https://github.com/hirashoaib/learn-javascript 
+// !variable
+// react kr rhy to class and function components, props, state, lifecycle methods, and hooks py zor 
+// Miss Hira Links
+// HTML  https://www.youtube.com/watch?v=57RKSZv-KQI&list=PLKvqnz8z1zWQ3BALy86tIXICkG874wAc6
+// CSS   https://www.youtube.com/watch?v=0lrAsPIWpZg&list=PLKvqnz8z1zWQSWIen_zUSEBmtqzPLuRob
+// 
+// 03037309909
+// 03001119288
+// https://chat.whatsapp.com/ERkmKpJWq9m2V2DfJJbW4e
+// https://chat.whatsapp.com/H3VWckYaTcw2X1lsvYmWgo 
+// https://chat.whatsapp.com/Bbt2dX5234QEVNVyDLz6g0
+// 
+
