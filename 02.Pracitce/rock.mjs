eb7c9fd6fb55c35046e3babd5f9bed6680a5e04b
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-10-23 16:46:09 +0500

    Rock Paper Scissor game made

--- 02.Pracitce/rock.mjs
+++ 02.Pracitce/rock.mjs
@@ -1,18 +1,15 @@
-//Efficent Rock,Paper,Scissors Game Code
+//Efficient Rock,Paper,Scissors Game Code
 // import promptSync from "prompt-sync";
 
 //                1        2           3
 const choices = ["Rock", "Paper", "Scissors"];
-const userNum = Number(
-  prompt("Enter 1 for Rock, 2 for Paper and 3 for Scissors")
-);
-console.log(userNum);
+const userNum = Number(prompt("Enter 1 for Rock, 2 for Paper and 3 for Scissors"));
 
 console.log(`Your choice was ${choices[userNum - 1]}`);
 
 const random = Math.floor(Math.random() * 3) + 1;
-console.log(random);
 console.log(`Computer's choice was ${choices[random - 1]}`);
+
 
 if (userNum === random) {
   console.log(`Draw`);
