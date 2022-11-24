ab7fffde2d42dcccc147a711686778a50c619524
Shehzad” <shehzad.dev@pm.me>
2022-11-25 02:36:51 +0500

    panaverse WEB 3 assignments notes

---
+++ web3AssignmentsNotes.js
@@ -0,0 +1,20 @@
+How to get GoerliETH on MetaMask?
+https://goerlifaucet.com/
+https://faucet.softbinator.com/
+https://faucet.paradigm.xyz/
+How to do all the Assignments of panaverse?
+1. Create your own Token:
+https://www.smartcontracts.tools/token.../create/ethereum/
+2. Provide Liquidity on Uniswap: 
+https://app.uniswap.org/#/pool
+3. Supply & Borrow on Aave:
+https://app.aave.com/
+4. Create your own Dao on Aragon Dao:
+https://client.aragon.org/#/
+5. Register your domain on ENS:
+https://app.ens.domains/
+
+asharib video
+https://www.facebook.com/groups/panaverse/permalink/5520755594682894/
+panaverse WEB 3 assignments
+https://youtu.be/33dI6j6vz90
