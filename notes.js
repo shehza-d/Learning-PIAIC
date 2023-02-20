907106125d5fed1e129c737fc62655e57987cc68
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-02-20 20:17:34 +0500

    Update notes.js

--- notes.js
+++ notes.js
@@ -182,4 +182,22 @@ Ap kisi ko wo chez nhi sheeka sagty jo usse phale se aati hy
 learn tailwind now with css
 complete git course from 10 perls when learning git in couse
 complete dpp-world course from when learning solidity in couse
+**************************************************
+NOTES
+Building Blocks of a Web Application
+There are a few things you need to consider when building modern applications. Such as:
+
+User Interface - how users will consume and interact with your application.
+Routing - how users navigate between different parts of your application.
+Data Fetching - where your data lives and how to get it.
+Rendering - when and where you render static or dynamic content. 
+Integrations - what third-party services you use (CMS, auth, payments, etc) and how you connect to them.
+Infrastructure - where you deploy, store, and run your application code (Serverless, CDN, Edge, etc).
+Performance - how to optimize your application for end-users.
+Scalability - how your application adapts as your team, data, and traffic grow.
+Developer Experience - your team's experience building and maintaining your application.
+
+08-Feb-2023
+complete panaverse website
+
 
